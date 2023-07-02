7910097a6f1943d4985a435e0f311997c97968d0
Jakob Winklehner <20190001@students.htl-perg.ac.at>
2023-07-02 22:44:35 +0200

    added cool picture 👍

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
+  title: "Portfolio from Jakob Winklehner",
   description: "Portfolio created by Jakob Winklehner",
 };
 
