d3f60f951a7716a4468ae05c1695fd0f8a008b33
Jakob Winklehner <20190001@students.htl-perg.ac.at>
2023-07-06 13:55:59 +0200

    centered card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -31,7 +31,7 @@ const Card: React.FC<CardProps> = ({
           className="fixed inset-0 flex items-center justify-center"
         >
           <div className="min-w-[800px] min-h-[500px] max-w-5xl mx-auto bg-white rounded-lg p-8 relative flex flex-col">
-            <div className="flex flex-col h-full">
+            <div className="flex flex-col grow h-full relative">
               <div className="flex justify-between mb-6">
                 <button
                   className="text-gray-500 hover:text-gray-700"
